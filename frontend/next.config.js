8aae028fcfe4bafd34f61c4b5630535f7ef61d45
sunagimo1998 <asstkm212038@gmail.com>
2024-09-15 02:15:07 +0900

    各種設定途中まで

---
+++ frontend/next.config.js
@@ -0,0 +1,10 @@
+/** @type{import('next').NextConfig} */
+constnextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    // styledComponentsの有効化
+    styledComponents: true,
+  },
+}
+
+module.exports = nextConfig
